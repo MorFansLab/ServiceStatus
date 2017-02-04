b573edb8af5fe4b7700c1e7c979af33d304cca58
SuperPaxxs <me@morfans.cn>
2017-02-04 11:37:55 +0800

    Commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm778633271-91c2e3c38545ecef70b88725', //MCN-博客
+	'm778633281-79e8585f44f142edbd86156b', //MCNN-博客
+	'm778633295-768f8f90b56e3066387e0a0c', //轻惋
+	'm778633291-52eb9cf04a775635208ca2c7', //MCC
+	'm778633899-c617d3535eebdb58683ec039', //API-everyday
+	'm778633898-e7f8f0f481963c6d51062bb8', //API-hitokoto	
+	'm778634030-1d03c553fa1512ec8f26b3ad'//MCC-yun
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
